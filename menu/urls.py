315af5beeafc177b77bf34e64bfd1cd633e43bdc
Nikita Provorov <nikiyorkovich@gmail.com>
2023-08-05 17:45:26 +0300

    Added urls

--- menu/urls.py
+++ menu/urls.py
@@ -1,10 +1,14 @@
+from django.urls import path
 from rest_framework import routers
 
-from menu.views import MenuViewSet
+from menu.views import MenuViewSet, RestaurantViewSet, get_most_voted_menu
 
 router = routers.DefaultRouter()
-router.register("", MenuViewSet)
+router.register("vote", MenuViewSet)
+router.register("restaurant", RestaurantViewSet)
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path("most-voted/", get_most_voted_menu),
+] + router.urls
 
 app_name = "menu"
